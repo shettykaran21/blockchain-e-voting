04eb63ca62ebe19d3feb265ea0a4fe663283a3e8
Karan Shetty <shettykaran2110@gmail.com>
2022-04-28 00:01:59 +0530

    Function to mint additional token

--- smart-contracts/contracts/DBVToken.sol
+++ smart-contracts/contracts/DBVToken.sol
@@ -2,11 +2,21 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import 'hardhat/console.sol';
 
 contract DBVToken is ERC20 {
+  address public admin;
+
   constructor(uint256 initialSupply)
     ERC20('Decentralized Blockchain Voting Token', 'DBV')
   {
-    _mint(msg.sender, initialSupply);
+    console.log('DBV Token Constructor called');
+    _mint(msg.sender, initialSupply * (10**decimals()));
+    admin = msg.sender;
+  }
+
+  function mint(address to, uint256 supply) external {
+    require(msg.sender == admin, 'Only admin can mint token');
+    _mint(to, supply);
   }
 }
