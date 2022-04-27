7d4fd92ff1f08b4851e1abc6f1c322666eb172e2
Karan Shetty <shettykaran2110@gmail.com>
2022-04-28 00:32:55 +0530

    Used Ownable contract in token

--- smart-contracts/contracts/DBVToken.sol
+++ smart-contracts/contracts/DBVToken.sol
@@ -2,21 +2,18 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import 'hardhat/console.sol';
 
-contract DBVToken is ERC20 {
-  address public admin;
-
+contract DBVToken is ERC20, Ownable {
   constructor(uint256 initialSupply)
     ERC20('Decentralized Blockchain Voting Token', 'DBV')
   {
-    console.log('DBV Token Constructor called');
+    // console.log('DBV Token Constructor called');
     _mint(msg.sender, initialSupply * (10**decimals()));
-    admin = msg.sender;
   }
 
-  function mint(address to, uint256 supply) external {
-    require(msg.sender == admin, 'Only admin can mint token');
+  function mint(address to, uint256 supply) external onlyOwner {
     _mint(to, supply);
   }
 }
