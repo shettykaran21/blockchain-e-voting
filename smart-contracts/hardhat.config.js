9ebc914c9255864480f3ad6549719e95bb6c4938
Karan Shetty <shettykaran2110@gmail.com>
2022-04-28 00:57:10 +0530

    Removed redundant code

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,13 +1,5 @@
 require('@nomiclabs/hardhat-waffle')
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
