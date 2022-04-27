fdc0b83a9bceff4018c0f1358e5e3991fe1fbeb3
Karan Shetty <shettykaran2110@gmail.com>
2022-04-28 01:00:18 +0530

    Test to mint initial tokens

---
+++ smart-contracts/test/token.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+describe('Mint token', function () {
+  it('Should mint DBV tokens', async function () {
+    const DBVToken = await ethers.getContractFactory('DBVToken')
+    const token = await DBVToken.deploy(1000)
+    await token.deployed()
+
+    const [owner] = await ethers.getSigners()
+    const ownerBalance = await token.balanceOf(owner.address)
+
+    const supply = await token.totalSupply()
+    const ethValue = ethers.utils.formatEther(supply)
+
+    console.log(ethValue)
+
+    expect(await token.totalSupply()).to.equal(ownerBalance)
+  })
+})
