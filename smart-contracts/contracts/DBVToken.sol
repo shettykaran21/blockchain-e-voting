4bb3d67c6e7f86c1bc61159a3a7e275d245d14e7
Karan Shetty <shettykaran2110@gmail.com>
2022-04-28 11:48:32 +0530

    Recommit dbvtoken

---
+++ smart-contracts/contracts/DBVToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import 'hardhat/console.sol';
+
+contract DBVToken is ERC20, Ownable {
+  address public contractOwner;
+
+  constructor(uint256 initialSupply)
+    ERC20('Decentralized Blockchain Voting Token', 'DBV')
+  {
+    // console.log('DBV Token Constructor called');
+    _mint(msg.sender, initialSupply * (10**decimals()));
+    contractOwner = msg.sender;
+  }
+
+  function mint(address to, uint256 supply) external onlyOwner {
+    require(
+      msg.sender == contractOwner,
+      'Only contract owner can mint additional tokens'
+    );
+    _mint(to, supply * (10**decimals()));
+  }
+}
