b7250d33d82978d0431065793c71013a5361e2a9
Karan Shetty <shettykaran2110@gmail.com>
2022-05-05 14:33:02 +0530

    Give voting rights

--- smart-contracts/contracts/Ballot.sol
+++ smart-contracts/contracts/Ballot.sol
@@ -31,10 +31,20 @@ contract Ballot {
   function vote(uint256 _candidate) external {
     Voter storage sender = voters[msg.sender];
     require(sender.weight != 0, 'Has no right to vote');
-    require(!sender.voted, 'Already voted.');
+    require(!sender.voted, 'Already voted');
     sender.voted = true;
     sender.vote = _candidate;
 
     candidates[_candidate].voteCount += sender.weight;
+  }
+
+  function giveVotingRight(address _voter) external {
+    require(
+      msg.sender == contractOwner,
+      'Only contract owner is allowed to give voting rights'
+    );
+    require(!voters[_voter].voted, 'The voter has already voted');
+    require(voters[_voter].weight == 0);
+    voters[_voter].weight = 1;
   }
 }
