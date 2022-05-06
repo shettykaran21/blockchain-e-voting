3f23e3261134db67a62d30261d2fcf5a5da24a2c
Karan Shetty <shettykaran2110@gmail.com>
2022-05-06 19:57:55 +0530

    Renamed test file

---
+++ smart-contracts/test/test-token.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+describe('Mint token', function () {
+  it('Should mint DBV tokens', async function () {
+    const DBVToken = await ethers.getContractFactory('DBVToken')
+    const token = await DBVToken.deploy(1000)
+    await token.deployed()
+
+    const [owner] = await ethers.getSigners()
+    const ownerBalance = await token.balanceOf(owner.address)
+
+    const supply = await token.totalSupply()
+    const ethValue = ethers.utils.formatEther(supply)
+    console.log(ethValue)
+
+    expect(await token.totalSupply()).to.equal(ownerBalance)
+  })
+
+  it('Should mint additional DBV tokens', async function () {
+    const DBVToken = await ethers.getContractFactory('DBVToken')
+    const token = await DBVToken.deploy(1000)
+    await token.deployed()
+
+    const [owner] = await ethers.getSigners()
+
+    await token.mint(owner.address, 100)
+    const ownerBalance = await token.balanceOf(owner.address)
+
+    const supply = await token.totalSupply()
+    const ethValue = ethers.utils.formatEther(supply)
+    console.log(ethValue)
+
+    expect(await token.totalSupply()).to.equal(ownerBalance)
+  })
+})
