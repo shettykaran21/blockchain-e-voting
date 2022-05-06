fd88e2d9654cfa81645e5042c8aac85395173da8
Karan Shetty <shettykaran2110@gmail.com>
2022-05-06 20:55:49 +0530

    Utiity function to convert from string to hex

--- smart-contracts/test/test-users.js
+++ smart-contracts/test/test-users.js
@@ -1,24 +1,21 @@
 const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
+const convertToHex = (str) =>
+  ethers.utils.hexZeroPad(
+    ethers.utils.hexlify(ethers.utils.toUtf8Bytes(str)),
+    32
+  )
+
 describe('Create User', function () {
   it('Should not create user without controller', async function () {
     const UserStorage = await ethers.getContractFactory('UserStorage')
     const storage = await UserStorage.deploy()
     await storage.deployed()
 
-    const username = ethers.utils.hexZeroPad(
-      ethers.utils.hexlify(ethers.utils.toUtf8Bytes('karan')),
-      32
-    )
-    const firstName = ethers.utils.hexZeroPad(
-      ethers.utils.hexlify(ethers.utils.toUtf8Bytes('Karan')),
-      32
-    )
-    const lastName = ethers.utils.hexZeroPad(
-      ethers.utils.hexlify(ethers.utils.toUtf8Bytes('Shetty')),
-      32
-    )
+    const username = convertToHex('karan')
+    const firstName = convertToHex('Karan')
+    const lastName = convertToHex('Shetty')
     const [owner] = await ethers.getSigners()
 
     expect(
